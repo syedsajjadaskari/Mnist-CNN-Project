79ec5fd8a7efceb9b3f95f03239ee4cd53e9f7de
Syed Sajjad Askari <84753561+syedsajjadaskari@users.noreply.github.com>
2025-11-20 02:17:52 +0000

    add the inferance cod e

--- inference.py
+++ inference.py
@@ -2,5 +2,32 @@ import os
 import sys
 import config 
 from models import MobileNetClassfier
-from utils import video_processor, preprocessing, visualization
+from utils import extract_frames, preprocess_batch, print_results_summary
+
+def classfify_video(video_path):
+    if os.path.exists(video_path):
+        print("The video not exits at path  ")
+        return
+
+    #extration of frames
+    frames, _ = extract_frames(video_path, num_frames=NUM_FRAMES)
+
+    #classfiy the object
+    classfier = MobileNetClassfier()
+    preprocessing = preprocess_batch(frames, target_size = config.INPUT_SIZE)
+    predictions = classfier.predict(preprocessing)
+
+    #results
+    print("results summmary")
+    print_results_summary(predictions, len(frames))
+
+
+if __name___ == '__main__':
+    if len(sys.argv) < 2:
+        print("python inference ")
+        sys.exit(1)
+
+    classfify_video(sys.argv[1])
+    
+
 
