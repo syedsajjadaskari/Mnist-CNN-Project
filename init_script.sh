bbb05488a4353391e57bb793c1fc925c918a7dbb
Syed Sajjad Askari <84753561+syedsajjadaskari@users.noreply.github.com>
2025-11-20 01:30:12 +0000

    update project library

--- init_script.sh
+++ init_script.sh
@@ -9,7 +9,7 @@ source env/bin/activate
 # Install packages (add your own here)
 pip install -r requirements_dev.txt
 # Freeze requirements
-pip freeze > requirements_dev.txt
+pip freeze > project_library.txt
 
 echo "Environment created and activated."
 echo "requirements.txt generated."
